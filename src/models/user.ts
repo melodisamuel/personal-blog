cad364adc00928239843e95182456dc00f3c5830
Melody <melodysamuelcodes@gmail.com>
2025-02-18 16:12:58 +0100

    #feat created user model

---
+++ src/models/user.ts
@@ -0,0 +1,21 @@
+import pool from "../../config/db";
+
+export interface User {
+    id?: number;
+    username: string;
+    password: string;
+    created_at?: Date;
+}
+
+
+// Find a user by username
+export async function findUserByUsername(username: string): Promise<User | null> {
+    const [rows] = await pool.query('SELECT * FROM users WHERE username = ?', [username]);
+    const users = rows as User[];
+    return users.length > 0 ? users[0] : null;
+}
+
+// Create a new user
+export async function creatUser(username: string, password: string): Promise<void> {
+    await pool.query('INSERT INTO users (username, password) VALUES (?, ?', [username, password]);
+}
