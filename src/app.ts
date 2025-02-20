aaf9af1780175f21d0897f666515b6e85ff5734d
Melody <melodysamuelcodes@gmail.com>
2025-02-20 10:22:52 +0100

    #feat setup the express app

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import session from 'express-session';
+import dotenv from 'dotenv';
+import routes from './routes/userRoutes';
+import path from 'path';
+
+dotenv.config();
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(session({ secret: process.env.SESSION_SECRET!, resave: false, saveUninitialized: false }));
+app.use(routes);
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
